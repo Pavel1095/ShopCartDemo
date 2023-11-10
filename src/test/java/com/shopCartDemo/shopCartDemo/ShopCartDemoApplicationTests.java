827eb64a852ec220edb0c896f2e275913d5f4d5b
Pavel_Gelfer <9153220501@mail.ru>
2023-11-10 19:43:43 +0300

    1st commit

---
+++ src/test/java/com/shopCartDemo/shopCartDemo/ShopCartDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopCartDemo.shopCartDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopCartDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
